5f96ea66667f36e7d3698c6b95e89c1de6047764
Gopinath <gopinathmannurkar7@gmail.com>
2026-03-27 17:04:55 +0530

    render fix

--- app.py
+++ app.py
@@ -53,5 +53,6 @@ def index():
 
     return render_template("index.html", result=result, assets=ASSETS)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
